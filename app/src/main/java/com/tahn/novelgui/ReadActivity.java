0a3604531c7f81c5627a0c1a0f76eae8106ba23e
tahn98 <nhatsieunhan999@gmail.com>
2019-05-07 20:48:42 +0700

    update5/7/2019

--- app/src/main/java/com/tahn/novelgui/ReadActivity.java
+++ app/src/main/java/com/tahn/novelgui/ReadActivity.java
@@ -2,18 +2,12 @@ package com.tahn.novelgui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ListView;
 
-public class NovelActivity extends AppCompatActivity {
-
-    ListView lvChapter;
+public class ReadActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_novel);
-
-        lvChapter = findViewById(R.id.lvChapter);
-
+        setContentView(R.layout.activity_read);
     }
 }
