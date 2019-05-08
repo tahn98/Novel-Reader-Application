7691172d08d4e2ae23b9ca3741c52a9fe16c53d9
tahn98 <nhatsieunhan999@gmail.com>
2019-05-08 21:11:08 +0700

    update5/8/2019

--- app/src/main/java/com/tahn/novelgui/NovelActivity.java
+++ app/src/main/java/com/tahn/novelgui/NovelActivity.java
@@ -1,17 +1,32 @@
 package com.tahn.novelgui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class NovelActivity extends AppCompatActivity {
 
-    ListView lvChapter;
+    TextView txtComment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_novel);
 
+        addControls();
+        txtComment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NovelActivity.this, CommentActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void addControls(){
+        txtComment = findViewById(R.id.txtComment);
     }
 }
